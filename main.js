9a1feaa3d0f906d4298e3f9fcb5c2dc55041bf6b
Ole <pranayamasimhamudra@gmail.com>
2021-10-16 23:45:52 +0500

    random damage and win title added

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const arenas = document.querySelector('.arenas');
+const arenas = document.querySelector(".arenas");
+const randomBtn = document.querySelector(".button");
+
 const player1 = {
     name: "Kitana",
     hp: 100,
@@ -19,22 +21,50 @@ const player2 = {
     },
 };
 
+randomBtn.addEventListener("click", function() {
+    changeHP(player1);
+    changeHP(player2);
+    if (player1.hp <= 0) {
+        arenas.appendChild(playerWin(player2.name));
+    } else if (player2.hp <= 0) {
+        arenas.appendChild(playerWin(player1.name));
+    }
+});
+
+function changeHP(player) {
+    const playerLife = document.querySelector(
+        ".player" + player.player + " .life"
+    );
+    player.hp -= Math.ceil(Math.random() * 20);
+    playerLife.style.width = player.hp + "%";
+
+    if (player.hp <= 0) {
+        playerLife.style.width = "0%";
+    }
+}
+
+function playerWin(name) {
+    const winTitle = createElement("div", "winTitle");
+    randomBtn.disabled = true;
+    winTitle.innerText = name + " wins";
+
+    return winTitle;
+}
+
 function createElement(tagName, className) {
     const tag = document.createElement(tagName);
-
     if (className) {
         tag.classList.add(className);
     }
-
     return tag;
 }
 
 function createPlayer(character) {
-    const player = createElement("div", 'player' + character.player);
-    const progressbar = createElement("div", 'progressbar');
-    const life = createElement("div", 'life');
-    const name = createElement("div", 'name');
-    const model = createElement("div", 'character');
+    const player = createElement("div", "player" + character.player);
+    const progressbar = createElement("div", "progressbar");
+    const life = createElement("div", "life");
+    const name = createElement("div", "name");
+    const model = createElement("div", "character");
     const imgModel = createElement("img");
 
     life.style.width = `${character.hp}%`;
