43bfe22c3ba440eb9497c8dadbb9ed2934a7aeb6
Ole <pranayamasimhamudra@gmail.com>
2021-10-25 19:12:25 +0500

    edit game initialization and charachters name

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-import { player1, player2 } from './assets/libs/characters.js';
-import generateLogs from './assets/libs/logs.js';
-import { createPlayer } from './assets/libs/createPlayer.js';
-import { roundFight } from './assets/libs/roundFight.js';
-import { checkWinner } from './assets/libs/roundEnd.js';
-import { $arenas, $formFight } from './assets/libs/gameScene.js';
-
-function gameStart(player1, player2) {
-    $arenas.append(createPlayer(player1), createPlayer(player2));
-    generateLogs('start', player1, player2);
-
-    $formFight.addEventListener('submit', function(e) {
-        e.preventDefault();
-        roundFight(player1, player2);
-        checkWinner(player1, player2);
-    });
-}
-
-gameStart(player1, player2);
+import Game from './assets/libs/Game.js';
+const game = new Game();
+game.start();
