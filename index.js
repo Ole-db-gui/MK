31b6286268d78143847abc16917a223532a4ad4c
Ole <pranayamasimhamudra@gmail.com>
2021-11-03 21:37:43 +0500

    fix arena generator for github pages

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function init() {
             el.classList.add("active");
 
             setTimeout(() => {
-                window.location.pathname = "./MK/arena.html";
+                window.open("https://ole-db-gui.github.io/MK/arena.html");
             }, 1000);
         });
 
